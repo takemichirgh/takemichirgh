37aabb83a9ae9f0aa07b684c5eb545888a286b5b
takemichirgh <takemichirgh@gmail.com>
2026-03-30 19:56:33 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,3 @@
-// firebase.js
-
-// Importar Firebase via CDN (para navegador)
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
@@ -14,9 +10,6 @@ import {
   onValue
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-
-// CONFIG DO SEU FIREBASE (já preenchido)
-
 const firebaseConfig = {
   apiKey: "AIzaSyDi1RRUhYN7eeT_l5JR-6QUp2Jq5OPIST0",
   authDomain: "takemichirgh-8346e.firebaseapp.com",
@@ -27,23 +20,10 @@ const firebaseConfig = {
   appId: "1:1027920659606:web:399e57e6fe5d078e5f2eb7"
 };
 
-
-// Inicializar Firebase
-
 const app = initializeApp(firebaseConfig);
-
-
-// Banco de dados
-
 const db = getDatabase(app);
 
-
-// Caminho onde os jogos ficam
-
 const jogosRef = ref(db, "jogos");
-
-
-// Exportar funções para usar no script.js
 
 export {
   db,
